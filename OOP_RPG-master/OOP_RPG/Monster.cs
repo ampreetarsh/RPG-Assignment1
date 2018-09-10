f6c03963c381a9d5fee8fb5ddea37ad67428628b
Arshpreet Kaur <arshpreetkaur989@gmail.com>
2018-09-10 10:50:50 -0500

    added contructor to monster class

--- OOP_RPG-master/OOP_RPG/Monster.cs
+++ OOP_RPG-master/OOP_RPG/Monster.cs
@@ -10,5 +10,15 @@ namespace OOP_RPG
         public int Defense { get; set; }
         public int OriginalHP { get; set; }
         public int CurrentHP { get; set; }
+
+
+        public Monster(string name, int strength, int defense, int originalHP, int currentHp)
+        {
+            this.Name = name;
+            this.Strength = strength;
+            this.Defense = defense;
+            this.OriginalHP = originalHP;
+            this.CurrentHP = currentHp;
+        }
     }
 }
