eff1e67fc6469c2c1514bbd292359a8bc37e2282
Arshpreet Kaur <arshpreetkaur989@gmail.com>
2018-09-11 13:31:56 -0500

    Added gold property

--- OOP_RPG-master/OOP_RPG/Monster.cs
+++ OOP_RPG-master/OOP_RPG/Monster.cs
@@ -10,6 +10,7 @@ namespace OOP_RPG
         public int Defense { get; set; }
         public int OriginalHP { get; set; }
         public int CurrentHP { get; set; }
+        public int Gold { get; set; }
 
 
         public Monster(string name, int strength, int defense, int originalHP, int currentHp)
@@ -19,6 +20,7 @@ namespace OOP_RPG
             this.Defense = defense;
             this.OriginalHP = originalHP;
             this.CurrentHP = currentHp;
+            this.Gold = 5;
         }
     }
 }
