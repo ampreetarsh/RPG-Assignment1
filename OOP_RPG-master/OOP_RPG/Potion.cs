b9a93d44d7ab6a104a32ae2218ceee3555af40c4
Arshpreet Kaur <arshpreetkaur989@gmail.com>
2018-09-12 13:24:52 -0500

    added an interface

--- OOP_RPG-master/OOP_RPG/Potion.cs
+++ OOP_RPG-master/OOP_RPG/Potion.cs
@@ -6,14 +6,18 @@ using System.Threading.Tasks;
 
 namespace OOP_RPG
 {
-    class Potion
+    class Potion : IItem
     {
         public int HP { get; set; }
         public string Name { get; set; }
+        public int OriginalValue { get; set; }
+        public int ResellValue { get; set; }
 
-        public Potion(int hp, string name) {
+        public Potion(int hp, string name, int originalValue, int resellValue) {
             this.HP = hp;
             this.Name = name;
+            this.OriginalValue = originalValue;
+            this.ResellValue = resellValue;
         }
     }
 }
--- OOP_RPG-master/OOP_RPG/Weapon.cs
+++ OOP_RPG-master/OOP_RPG/Weapon.cs
@@ -1,14 +1,18 @@
 using System;
 namespace OOP_RPG
 {
-    public class Weapon
+    public class Weapon : IItem
     {
-        public Weapon(string name, int strength) {
+        public Weapon(string name, int strength, int originalValue, int resellValue) {
             this.Name = name;
             this.Strength = strength;
+            this.OriginalValue = originalValue;
+            this.ResellValue = resellValue;
         }
         
         public string Name { get; set; }
         public int Strength { get; set; }
+        public int OriginalValue { get; set; }
+        public int ResellValue { get; set; }
     }
 }
