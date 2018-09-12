f0bef7c81214e015951b727033f3296eef63d526
Arshpreet Kaur <arshpreetkaur989@gmail.com>
2018-09-12 11:29:21 -0500

    added shop and potion class

---
+++ OOP_RPG-master/OOP_RPG/Potion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_RPG
+{
+    class Potion
+    {
+        public int HP { get; set; }
+        public string Name { get; set; }
+
+        public Potion(int hp, string name) {
+            this.HP = hp;
+            this.Name = name;
+        }
+    }
+}
---
+++ OOP_RPG-master/OOP_RPG/Shop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_RPG
+{
+    class Shop
+    {
+        List<Weapon> weapons { get; set; }
+        List<Armor> armor { get; set; }
+        List<Potion> potion { get; set; }
+
+        public Shop()
+        {
+            this.weapons = new List<Weapon>();
+            this.armor = new List<Armor>();
+            this.potion = new List<Potion>();
+        }
+    }
+}
